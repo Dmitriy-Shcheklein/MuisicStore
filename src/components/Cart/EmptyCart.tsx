2733473e845f7beb349631bb3f3a399e08def14a
Dmitriy-Shcheklein <dima.sheklein@yandex.ru>
2021-10-14 11:43:14 +0500

    Change a message if cart is empty

--- src/components/Cart/EmptyCart.tsx
+++ src/components/Cart/EmptyCart.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
+import { Container, TextField } from '../../styled/styled'
 
 const EmptyCart: FC = () => {
   return (
 
-    <h2>Your shopping cart is empty.</h2>
+    <Container fldir='column' juscontent='center' style={{ height: '80vh' }}>
+      <TextField size='5rem' style={{ color: '#3f51b5' }}>
+        You have not selected anything<br />
+        Cart is empty &#128561;
+      </TextField>
+    </Container>
 
   )
 }
